2616babad807ac906344649a006bec3ad8ba6095
Brehan25885 <brehan.ibrahim@gmail.com>
2018-04-28 11:15:33 +0200

    fixes in authentication and middlewares

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('/home',function () {
 })->name('home');
 
 
-Route::group(['middleware' =>['role:receptionist']],function(){ 
+Route::group(['middleware' =>['auth'],['role:receptionist']],function(){ 
 Route::get('/indexrecep',function () {
     return view('recep.index');  
 })->name('indexrecep');
@@ -53,7 +53,7 @@ Route::get('recep/getdata','ReceptionistController@getdata')->name('datatables.d
  });
  
  
- Route::group(['middleware' =>['role:admin']],function(){ 
+ Route::group(['middleware' =>['auth'],['role:admin']],function(){ 
     Route::get('/indexManager','AdminController@indexManager')->name('tables');
     Route::get('manager/getData','AdminController@getDataManager')->name('tables.data');
     Route::get('/indexRece','AdminController@indexRece')->name('datatable');
@@ -76,14 +76,9 @@ Route::get('recep/getdata','ReceptionistController@getdata')->name('datatables.d
      */
     Route::get('/admins/{id}/deletec','AdminController@destroyClient');
     Route::get('/admins/{id}/deletem','AdminController@destroyManager');
-    
-    
-    
     Route::get('ajaxdata/removedatam', 'AdminController@removedataManager')->name('tables.datadestroyManager');
     Route::get('ajaxdata/removedatac', 'AdminController@removedataClient')->name('tables.datadestroyClient');
     Route::get('ajaxdata/removedatar', 'AdminController@removedataRece')->name('tables.datadestroyRece');
-    
-    
      Route::get('/admins/{id}/deleter','AdminController@destroyRece');    
      });
 
